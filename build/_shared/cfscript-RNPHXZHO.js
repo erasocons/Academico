ccb4dcbde0044577761716644f85b149a49345ec
erasocons <erasocons@users.noreply.github.com>
2026-08-19 01:44:58 +0000

    deploy: 575d1b64bd16c8b02f5e02c8991e334d9afcb49c

--- build/_shared/cfscript-RNPHXZHO.js
+++ build/_shared/cfscript-RNPHXZHO.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-XVFFZD2L.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/Academico//build/_shared/chunk-XVFFZD2L.js";import"/Academico//build/_shared/chunk-OZE3FFNP.js";export default a();
